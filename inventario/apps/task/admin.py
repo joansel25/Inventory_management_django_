cdf1176b61fb1c1ebd0da468b656388a8f9eb8fe
Laura Osorio <Lauraosorioj7@gmail.com>
2025-09-13 02:01:47 -0500

    Agrega modelos, configuración de admin y migración inicial

--- inventario/apps/task/admin.py
+++ inventario/apps/task/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente, Empleado
 
+admin.site.register(Cliente)
+admin.site.register(Empleado)
 # Register your models here.
+
+##Si aca coloco cliente pero en los modelos debe de ser plurar, en el admin me sale con doble SS?
+
--- inventario/apps/task/models.py
+++ inventario/apps/task/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# CLIENTES
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+# EMPLEADOS
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
