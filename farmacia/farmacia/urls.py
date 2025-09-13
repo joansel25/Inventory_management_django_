0e2abddcf4929eebe5bd84fc1e1fce3d01a2dd76
joansel25 <joan.cardenas@correo.tdea.edu.co>
2025-09-12 20:10:03 -0500

    Version 1.2 Management-Inventory-Django

--- farmacia/farmacia/urls.py
+++ farmacia/farmacia/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('farmacia/', include('apps.task.urls'))#agregar las rutas configuradas en la api para que el servidor las incluyas al inciar
 ]
